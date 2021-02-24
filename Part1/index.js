479f9ac057737d203ea1e8b7032a504f6029b750
djkoser <koser.david@gmail.com>
2021-02-24 17:31:27 -0600

    Complete remaining black diamond challenges

--- Part1/index.js
+++ Part1/index.js
@@ -1,6 +1,6 @@
 console.log("Test 123");
 
-let progress = [1,1,1,1,1,1,1,1,1];
+let progress = [1, 1, 1, 1, 1, 1, 1, 1, 1];
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,35 +15,40 @@ const winningCombos = [
 const play = (clickedID) => {
     let playerSpan = document.getElementById("player");
     let clickedElement = document.getElementById(clickedID)
-    if (playerSpan.innerText === 'X') {
-        playerSpan.innerText = 'O';
-        clickedElement.innerText = 'X';
-        progress[clickedID] = 'X';
-        console.log(progress);
+    if (progress[clickedID] === 1) {
+        if (playerSpan.innerText === 'X') {
+            playerSpan.innerText = 'O';
+            clickedElement.innerText = 'X';
+            progress[clickedID] = 'X';
+            console.log(progress);
 
-    } else {
-        playerSpan.innerText = 'X';
-        clickedElement.innerText = 'O';
-        progress[clickedID] = 'O'
-        console.log(progress);
-    };
-    for (let i = 0; i < winningCombos.length; i++) {
-        let countX = 0;
-        let countO = 0;
-        for (a = 0; a < winningCombos[i].length; a++) {
-            if (progress[winningCombos[i][a]] === "X") {
-                countX += 1;
-            } else if (progress[winningCombos[i][a]] === "O") {
-                countO += 1;
-            };
+        } else {
+            playerSpan.innerText = 'X';
+            clickedElement.innerText = 'O';
+            progress[clickedID] = 'O'
+            console.log(progress);
         };
-        if (countX === 3) {
-            return window.alert("Player X is the Winner!")
-        } else if (countO === 3) {
-            return window.alert("Player O is the Winner!");
-        } else if (progress.includes(1)===false && countX != 3 && countO != 3) {
-            return window.alert("CAT's Game!");
-        }
+        for (let i = 0; i < winningCombos.length; i++) {
+            let countX = 0;
+            let countO = 0;
+            for (a = 0; a < winningCombos[i].length; a++) {
+                if (progress[winningCombos[i][a]] === "X") {
+                    countX += 1;
+                } else if (progress[winningCombos[i][a]] === "O") {
+                    countO += 1;
+                };
+            };
+            if (countX === 3) {
+                window.alert("Player X is the Winner!")
+                return reset()
+            } else if (countO === 3) {
+                window.alert("Player O is the Winner!");
+                return reset()
+            } else if (progress.includes(1) === false && countX != 3 && countO != 3) {
+                window.alert("CAT's Game!");
+                return reset();
+            }
+        };
     };
 };
 
