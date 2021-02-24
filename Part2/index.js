78d351a042fd1bd86ef174e9b5ae98c0192afe96
djkoser <koser.david@gmail.com>
2021-02-24 17:23:48 -0600

    Change to clear form entry on submit for part 2

--- Part2/index.js
+++ Part2/index.js
@@ -6,10 +6,12 @@ let cardList = document.getElementsByClassName("cards");
 
 const setCard = () => {
     document.getElementById(cardIDInput.value).style.backgroundColor=styleInput.value;
+    cardIDInput.value="diamonds";
+    styleInput.value="";
 }
 
 const clearCards = () => {
     for (i=0; i<cardList.length; i++) {
         cardList[i].style.backgroundColor="white";
-    }
-}
+    };
+};
